94072e007ded44b8dbbf76e19f11d41f8e116516
Faizan Zafar <faizanzafar40@gmail.com>
2017-07-11 14:36:26 +0500

    Add files via upload

---
+++ Decoder.py
@@ -0,0 +1,65 @@
+from PIL import Image, ImageFilter
+import binascii
+
+from HelperFunctions import *
+
+#################################################
+#SET IMAGE FILENAME HERE WHICH YOU WANT TO DECODE
+#################################################
+imageName = '1modified.png'
+
+#open image
+image = Image.open(imageName)
+
+#store size
+length,height = image.size
+
+#split into three bands
+imageLoaded = image.load()
+
+index = 0
+counter = 0
+
+decodedBin = ''
+
+#loop over all pixel values
+for x in range(0,height-1):
+  for y in range(0,length-1):
+
+    #decode one bit from every byte of data
+    #stop when terminating 0000s are found
+    r,g,b = imageLoaded[y,x]
+
+    if counter != 35:
+      bit = r & 1
+      decodedBin = decodedBin + str(bit)
+      if bit == 0:
+        counter = counter + 1
+      else:
+        counter = 0
+    else:
+      break
+
+    if counter != 35:
+      bit = g & 1
+      decodedBin = decodedBin + str(bit)
+      if bit == 0:
+        counter = counter + 1
+      else:
+        counter = 0
+    else:
+      break
+
+    if counter != 35:
+      bit = b & 1
+      decodedBin = decodedBin + str(bit)
+      if bit == 0:
+        counter = counter + 1
+      else:
+        counter = 0
+    else:
+      break
+
+decodedBin = decodedBin[0:len(decodedBin)-counter-1] #remove the terminating bits we put earlier while encoding data
+#print(decodedBin) #for debugging uncomment this line
+print(return_text(decodedBin))
---
+++ Encoder.py
@@ -0,0 +1,64 @@
+from PIL import Image, ImageFilter
+import binascii
+import random
+
+from HelperFunctions import *
+
+def myEncoder(fileName,number):
+    with open(fileName, "r") as myfile:
+        data=myfile.read()
+
+    testBin = return_binary(data) + '10000000000000000000000000000000000000000'
+    binLength = len(testBin)
+
+    #open image
+    image = Image.open(str(random.randrange(1, 6)) + '.png')
+
+    #store size
+    length,height = image.size
+
+    #split into three bands
+    imageLoaded = image.load()
+
+    #start encoding from the first bit
+    index = 0
+
+    #loop over all pixel values
+    for x in range(0,height-1):
+        for y in range(0,length-1):
+
+            r,g,b = imageLoaded[y,x]
+            #encoding in RED
+            if index < binLength:
+                if testBin[index] == '0':
+                    r = clear_last_bit(r)
+                else:
+                    r = set_last_bit(r)
+                index = index + 1
+            else:
+                break
+
+            #encoding in GREEN
+            if index < binLength:
+                if testBin[index] == '0':
+                    g = clear_last_bit(g)
+                else:
+                    g = set_last_bit(g)
+                index = index + 1
+            else:
+                break
+
+            #encoding in BLUE
+            if index < binLength:
+                if testBin[index] == '0':
+                    b = clear_last_bit(b)
+                else:
+                    b = set_last_bit(b)
+                index = index + 1
+            else:
+                break
+
+            imageLoaded[y,x] = (r, g, b)
+
+    #Saving the image
+    image.save(number + 'modified.png', 'PNG')
